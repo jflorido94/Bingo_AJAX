a6f7b4752f11e4aa4f72bd5a1d69835f656b69c0
jflorido94 <hbranky22@hotmail.com>
2020-02-21 18:08:55 +0100

    carton creado

--- js/carton.js
+++ js/carton.js
@@ -3,27 +3,27 @@ class Carton {
   
   constructor(){
   
-    var lineas = [[],[],[]];
-    var columnas = [];
+    this.lineas = [[],[],[]];
+    this.columnas = [];
 
   
     // nueve columnas en cada carton
     for (let j = 0; j<9; j++) {
+      let columna = [];
       // tres numeros al azar (en el rango segun columna) por columna
       for (let i = 0; i < 3; i++) {
-        var columna = new Array();
         do {
           var numero = this.getRandomInt(1+10*j,11+10*j)
         } while (columna.includes(numero));
         columna.push(numero);
       }
       // agregamos la columna completa y ordenada al carton
-      columnas.push(columna.sort());
+      this.columnas.push(columna.sort());
     }
     // Equipara el carton mostrado por columnas por el carton mostrado por filas
-    for (let i = 0; i < columnas.length; i++) {
-      for (let j = 0; j < columnas[i].length; j++) {
-        lineas[j][i]=columnas[i][j];
+    for (let i = 0; i < this.columnas.length; i++) {
+      for (let j = 0; j < this.columnas[i].length; j++) {
+        this.lineas[j][i]=this.columnas[i][j];
       }
     }
     //quitamos las 4 casillas por cada fila del carton
@@ -31,15 +31,20 @@ class Carton {
   }
 
   casillasOcultas(){
-    var quitar= [[],[],[]];
+    let quitar= new Array(3);
+    quitar[0]=new Array();
+    quitar[1]=new Array();
+    quitar[2]=new Array();
+    let posicion= 0;
+    let coinciden = false;
     do {
-      var coinciden = false;
+      coinciden = false;
       // por cada fila del carton tachamos 4 casillas
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j<4; j++){
           do {
           // quitar la posicion random
-          var posicion=this.getRandomInt(0,9);
+          posicion=this.getRandomInt(0,9);
           } while (quitar[i].includes(posicion))
           // si no existe en las que quitaremos de la fila i, la añadimos, 
           // asi hasta ser 4 las casillas quitadas
@@ -57,8 +62,8 @@ class Carton {
 
     for (let i = 0; i <quitar.length; i++) {
       for (let j = 0; j < quitar[i].length; j++) {     
-        this.lineas[i][quitar[i][j]]="X";
-        this.columnas[quitar[i][j]][i]="x";
+        this.lineas[i][quitar[i][j]]=0;
+        this.columnas[quitar[i][j]][i]=0;
       }
     }
   }
